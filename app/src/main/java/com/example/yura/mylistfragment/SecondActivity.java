24ca0311cf1ffcb9b7f2a42d38a2687c6dc5ddad
Yuriy Ivanov <yuriygr@yahoo.com>
2018-10-21 08:11:08 +0300

    commit

---
+++ app/src/main/java/com/example/yura/mylistfragment/SecondActivity.java
@@ -0,0 +1,24 @@
+package com.example.yura.mylistfragment;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class SecondActivity extends AppCompatActivity  {
+    public String flag;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        flag = getIntent().getStringExtra("flag");
+        setContentView(R.layout.activity_second);
+    }
+
+    public String GetFlag() {
+        return flag;
+    }
+
+
+
+}
